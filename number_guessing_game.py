5e6cb1301883b892efd0ac0081029a61955e03ed
Haro Abdulah <haro9502@gmail.com>
2024-01-09 22:27:12 +0200

    added error handling

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,23 +1,32 @@
 import random
 
-attempts = 5
-
 print()
 print('***** Welcome to the number guessing game ***** \n')
 print('I have selected a number between 1 and 100. Try to guess it in 5 attempts.')
 
-secret_number = random.randint(1, 100)
-for attempt in range(attempts):
-    guess = int(input('Enter your guess: '))
+attempts = 5
+while attempts != 0:
+    
+    try:
+        secret_number = random.randint(1, 100)
+        for attempt in range(attempts):
+            guess = int(input('Enter your guess: '))
+            
+            if guess.is_integer:
+                attempts -= 1
 
-    if guess == secret_number:
-        print(f'Congratulations! You guessed the correct number in {attempt + 1} attempts')
-        break
+            if guess == secret_number:
+                print(f'Congratulations! You guessed the correct number in {attempt + 1} attempts')
+                break
 
-    elif guess < secret_number:
-        print('Your number is too low, try again!')
-    elif guess > secret_number:
-        print('Your number is too high, try again!')
+            elif guess < secret_number:
+                print('Your number is too low, try again!')
+            elif guess > secret_number:
+                print('Your number is too high, try again!')
+    
+        else:
+            print(f"\nSorry you've ran out of attempts. The correct number is {secret_number}. Try again.")
+            print()
 
-else:
-    print(f"\nSorry you've ran out of attempts. The correct number is {secret_number}. Try again.")
+    except ValueError:
+        print('Enter a valid number between 1 and 100!')
